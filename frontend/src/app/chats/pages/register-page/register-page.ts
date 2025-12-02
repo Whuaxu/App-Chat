3aa71d514fa8e5ecddb70202585c60c827bb25f7
javier.pena <javierpb6@gmail.com>
2025-12-02 19:31:28 +0100

    Added register page

---
+++ frontend/src/app/chats/pages/register-page/register-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  imports: [],
+  templateUrl: './register-page.html',
+  styleUrls: ['./register-page.scss'],
+})
+export default class RegisterPage { }
