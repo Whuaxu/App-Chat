30dba124ccf182c7475665cfe61f47442505f47f
javier.pena <javierpb6@gmail.com>
2025-12-02 18:40:18 +0100

    Added new routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        loadComponent: () => import('./chats/pages/login-page/login-page'),
+    },
+
+    {
+        path: 'chat',
+        loadComponent: () => import('./chats/pages/chat-page/chat-page'),
+    },
+
+    {
+        path: 'register',
+        loadComponent: () => import('./chats/pages/register-page/register-page'),
+    },
+
+    {
+        path: '**',
+        redirectTo: '',
+    }
+];
