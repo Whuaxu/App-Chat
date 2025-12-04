959945948aaea18bc3c589f77a64a3103e8c00c6
javier.pena <javierpb6@gmail.com>
2025-12-04 17:16:39 +0100

    Added users controller

---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,172 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getModelSchemaRef,
+  patch,
+  put,
+  del,
+  requestBody,
+  response,
+} from '@loopback/rest';
+import {User} from '../models';
+import {UserRepository} from '../repositories';
+import {validateCredentials} from '../services/validator';
+import * as lodash from 'lodash';
+import { inject } from '@loopback/core';
+import { BcryptHasher } from '../services/hashPass.bcrypt';
+
+export class UserController {
+  constructor(
+    @repository(UserRepository)
+    public userRepository : UserRepository,
+    @inject('services.hasher')
+    public hasher: BcryptHasher,
+  ) {}
+
+  @post('/users')
+  @response(200, {
+    description: 'User model instance',
+    content: {'application/json': {schema: getModelSchemaRef(User)}},
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {
+            title: 'NewUser',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    user: Omit<User, 'id'>,
+  ): Promise<User> {
+    return this.userRepository.create(user);
+  }
+
+  @get('/users/count')
+  @response(200, {
+    description: 'User model count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async count(
+    @param.where(User) where?: Where<User>,
+  ): Promise<Count> {
+    return this.userRepository.count(where);
+  }
+
+  @get('/users')
+  @response(200, {
+    description: 'Array of User model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(User, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(User) filter?: Filter<User>,
+  ): Promise<User[]> {
+    return this.userRepository.find(filter);
+  }
+
+  @patch('/users')
+  @response(200, {
+    description: 'User PATCH success count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {partial: true}),
+        },
+      },
+    })
+    user: User,
+    @param.where(User) where?: Where<User>,
+  ): Promise<Count> {
+    return this.userRepository.updateAll(user, where);
+  }
+
+  @get('/users/{id}')
+  @response(200, {
+    description: 'User model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(User, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.string('id') id: string,
+    @param.filter(User, {exclude: 'where'}) filter?: FilterExcludingWhere<User>
+  ): Promise<User> {
+    return this.userRepository.findById(id, filter);
+  }
+
+  @patch('/users/{id}')
+  @response(204, {
+    description: 'User PATCH success',
+  })
+  async updateById(
+    @param.path.string('id') id: string,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {partial: true}),
+        },
+      },
+    })
+    user: User,
+  ): Promise<void> {
+    await this.userRepository.updateById(id, user);
+  }
+
+  @put('/users/{id}')
+  @response(204, {
+    description: 'User PUT success',
+  })
+  async replaceById(
+    @param.path.string('id') id: string,
+    @requestBody() user: User,
+  ): Promise<void> {
+    await this.userRepository.replaceById(id, user);
+  }
+
+  @del('/users/{id}')
+  @response(204, {
+    description: 'User DELETE success',
+  })
+  async deleteById(@param.path.string('id') id: string): Promise<void> {
+    await this.userRepository.deleteById(id);
+  }
+
+  @post('/signup')
+  @response(200, {
+    description: 'Signup a new user',
+    content: {'application/json': {schema: getModelSchemaRef(User)}},
+  })
+  
+  async signup(@requestBody() userData: User) {
+
+    userData.password = await this.hasher.hashPassword(userData.password);
+    
+    validateCredentials(lodash.pick(userData, ['email', 'password']));
+    const savedUser = await this.userRepository.create(userData);
+    return savedUser;
+
+  }
+}
