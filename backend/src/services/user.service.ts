c90feb667c404389bab85d81c834ee17ad7d793c
javier.pena <javierpb6@gmail.com>
2025-12-04 18:17:54 +0100

    Added user service logic

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,16 +1,52 @@
 import { UserService } from "@loopback/authentication";
 import { User } from "../models";
-import { Credentials } from "../repositories/user.repository";
-import { UserProfile } from "@loopback/security";
+import { Credentials, UserRepository } from "../repositories/user.repository";
+import { securityId, UserProfile } from "@loopback/security";
+import { repository } from "@loopback/repository";
+import { HttpErrors } from "@loopback/rest";
+import { inject } from "@loopback/core";
+import { BcryptHasher } from "./hashPass.bcrypt";
 
 export class myUserService implements UserService<User, Credentials> {
 
-    verifyCredentials(credentials: Credentials): Promise<User> {
-        throw new Error("Method not implemented.");
-    }
-    
-    convertToUserProfile(user: User): UserProfile {
-        throw new Error("Method not implemented.");
+    constructor(
+        @repository(UserRepository)
+        public userRepository: UserRepository,
+        @inject('services.hasher')
+        public hasher: BcryptHasher,
+    ){}
+
+    async verifyCredentials(credentials: Credentials): Promise<User> {
+        
+        const foundUser = await this.userRepository.findOne({
+            where: {
+                email: credentials.email,
+            }
+        });
+
+        if(!foundUser){
+            throw new HttpErrors.NotFound(`User not found with this ${credentials.email}`);
+        }
+
+        const passwordMatched = await this.hasher.comparePassword(credentials.password, foundUser.password);
+        
+        if(!passwordMatched){
+            throw new HttpErrors.Unauthorized('Password is not valid');
+        }
+
+        return foundUser;
     }
 
+    convertToUserProfile(user: User): UserProfile {
+        
+        let userName = '';
+        if(user.firstName){
+            userName = user.firstName;
+        }
+
+        return {
+            [securityId]: `${user.id}`,
+            name: userName
+        };
+    }
 }
