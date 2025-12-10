d24559a818806a7a80ffb2461d519507ba5feb63
javier.pena <javierpb6@gmail.com>
2025-12-10 11:40:44 +0100

    Added message logic

--- backend/src/repositories/user.repository.ts
+++ backend/src/repositories/user.repository.ts
@@ -1,7 +1,9 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, HasManyRepositoryFactory} from '@loopback/repository';
 import {MongoDataSource} from '../datasources';
-import {User, UserRelations} from '../models';
+import {User, UserRelations, Message} from '../models';
+import {MessageRepository} from './message.repository';
+
 
 export type Credentials = {
   email: string;
@@ -13,9 +15,28 @@ export class UserRepository extends DefaultCrudRepository<
   typeof User.prototype.id,
   UserRelations
 > {
+  public readonly sentMessages: HasManyRepositoryFactory<
+    Message,
+    typeof User.prototype.id
+  >;
+
   constructor(
     @inject('datasources.mongo') dataSource: MongoDataSource,
+    @repository.getter('MessageRepository')
+    protected messageRepositoryGetter: Getter<MessageRepository>,
   ) {
     super(User, dataSource);
+    this.sentMessages = this.createHasManyRepositoryFactoryFor(
+      'sentMessages',
+      messageRepositoryGetter,
+    );
+    this.registerInclusionResolver(
+      'sentMessages',
+      this.sentMessages.inclusionResolver,
+    );
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.findOne({where: {email}});
   }
 }
